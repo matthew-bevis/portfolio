352d3b89d99518dc37858d0b1c68f28aaa2fc62c
mb19bo <89614527+mb19bo@users.noreply.github.com>
2024-04-10 18:14:41 -0400

    vercel bug testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { ThemeProvider} from './context/themeContext';
 import HomePage from './home/page';
-import 'global.css';
+import './global.css';
 
 function Page() {
     return (
