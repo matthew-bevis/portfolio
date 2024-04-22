f40f0a9613e1ae171c91488e5fbcf3a74ae629e2
mb19bo <89614527+mb19bo@users.noreply.github.com>
2024-04-22 15:52:40 -0400

    email update

--- app/api/sendEmail.tsx
+++ app/api/sendEmail.tsx
@@ -1,53 +1,40 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import mailchimpTransactional from '@mailchimp/mailchimp_transactional';
 
 dotenv.config();
+
+const mailchimpClient = mailchimpTransactional({ apiKey: process.env.API_KEY });
 
 export default async function sendEmail(req: VercelRequest, res: VercelResponse) {
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // Another common practice is to restrict this to certain origins:
-    // res.setHeader('Access-Control-Allow-Origin', 'https://yourdomain.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
     if (req.method === 'OPTIONS') {
-        // Handle OPTIONS Method for CORS preflight
         return res.status(200).end();
     }
 
     if (req.method !== 'POST') {
-        // Handle actual request
         res.setHeader('Allow', ['POST']);
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
 
     const { name, email, company, message } = req.body;
 
-    const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
-
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: 'matthew-bevis@comcast.net',
-        subject: 'New message from your website!',
-        text: `Name: ${name}\nEmail: ${email}\nCompany: ${company}\nMessage: ${message}`
+    const messageContent = {
+        from_email: process.env.EMAIL_USER,
+        subject: "New message from your website!",
+        text: `Name: ${name}\nEmail: ${email}\nCompany: ${company}\nMessage: ${message}`,
+        to: [{ email: 'matthew-bevis@comcast.net', type: 'to' }]
     };
 
     try {
-        await transporter.sendMail(mailOptions);
-        res.status(200).json({ message: 'Email sent successfully' });
+        const response = await mailchimpClient.messages.send({ message: messageContent });
+        res.status(200).json({ message: 'Email sent successfully', response });
     } catch (error) {
-        res.status(500).json({ error: 'Failed to send email' });
+        res.status(500).json({ error: 'Failed to send email', details: error });
     }
 }
